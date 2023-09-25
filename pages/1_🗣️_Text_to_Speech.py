64dc9fca30b50809b8fd4aff0aaad9254a2c5ed7
Meowmeow-alt <baotiendancer@gmail.com>
2023-09-25 09:00:22 +0700

    Update functions

--- pages/1_🗣️_Text_to_Speech.py
+++ pages/1_🗣️_Text_to_Speech.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import gtts
+from essential_generators import DocumentGenerator,MarkovWordGenerator
+import IPython
+from textblob import TextBlob
+from googletrans import Translator
+
+gen = DocumentGenerator(word_generator=MarkovWordGenerator())
+translator = Translator()
 
 #___________SIDE BAR____________
 
@@ -12,8 +19,8 @@ st.sidebar.info(
     )
 
 def css(file):
-     with open(file) as f:
-          st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    with open(file) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 css("style/style.css")
 
 #___________MAIN CONTENT____________
@@ -22,24 +29,33 @@ st.title('CONVERT TEXT TO SPEECH')
 st.write('###')
 
 def main():
-     text = text_()
-     st.write('###')
-     language = language_()
-     st.write('###')
-     speed = speed_()
-     st.write('###')
-     try:
-          converting(text,language,speed)
-     except AssertionError:
-          pass
+    while True:
+        try:
+            text = st.text_input("Your text to convert: ")
+            if not text:
+                text = gen.sentence()
+            st.write('###')
 
-# text to convert
-def text_():
-     text = st.text_input('Your text to convert:')
-     return text
+            lang = language()
+            st.write('###')
+
+            speed = ask_speed()
+            st.write('###')
+
+            convert(text,lang,speed)
+            st.write('###')
+
+            sentiment_detection(text)
+            break
+        except EOFError:
+            break
+        except:
+            pass
+
 
 # language
-lang_choice = {'Afrikaans':'af','Arabic':'ar','Bengali':'bn','Bosnian':'bs','Catalan':'ca','Czech':'cs',
+def language(l):
+    lang_choices = {'Afrikaans':'af','Arabic':'ar','Bengali':'bn','Bosnian':'bs','Catalan':'ca','Czech':'cs',
     'Welsh':'cy','Danish':'da','German':'de','Greek':'el','English':'en','Esperanto':'eo','Spanish':'es',
     'Estonian':'et','Finnish':'fi','French':'fr','Gujarati':'gu','Hindi':'hi','Croatian':'hr','Hungarian':'hu',
     'Armenian':'hy','Indonesian':'id','Icelandic':'is','Italian':'it','Japanese':'ja','Javanese':'jw','Khmer':'km',
@@ -49,24 +65,35 @@ lang_choice = {'Afrikaans':'af','Arabic':'ar','Bengali':'bn','Bosnian':'bs','Cat
     'Swedish':'sv','Swahili':'sw','Tamil':'ta','Telugu':'te','Thai':'th','Filipino':'tl','Turkish':'tr',
     'Ukrainian':'uk','Urdu':'ur','Vietnamese':'vi','Chinese':'zh-CN'}
 
-def language_():
-     choice = st.selectbox('Choose a language:', lang_choice.keys())
-     language = lang_choice[choice]
-     return language
+    choice = st.selectbox('Choose a language:', lang_choices.keys())
+    language = lang_choices[choice]
+    return language
 
 # speed
-def speed_():
-     speed = st.radio(
-          "Do you want to hear the speech slowly?",
-          [":rainbow[YES PLEASE]", "NO THANKS"])
-     if speed == ':rainbow[YES PLEASE]':
-          return True
-     return False
+def ask_speed():
+    speed = st.radio(
+        "Do you want to hear the speech slowly?",
+        [":rainbow[YES PLEASE]", "NO THANKS"])
+    if speed == ':rainbow[YES PLEASE]':
+        return True
+    return False
 
 # Converting
-def converting(text, language, speed):
-     result = gtts.gTTS(text,lang=language,slow=speed)
-     result.save('result.mp3')
-     st.audio('result.mp3')
+def convert(text,lang,speed):
+    result = gtts.gTTS(text=text, lang=lang, slow=speed)
+    result.save('audio.mp3')
+    IPython.display.Audio("audio.mp3")
+
+def sentiment_detection(text):
+    class_output = translator.translate(text, dest='en')
+    text = getattr(class_output, 'text')
+
+    analyze = TextBlob(text)
+    rate = analyze.sentiment.polarity
+    if rate == 0:
+        return st.info('Sentiment analysis: Your text is neutral')
+    elif rate > 0:
+        return st.success('Sentiment analysis: Your text is positive')
+    return st.warning('Sentiment analysis: Your text is negative')
 
 main()
